5fc5592a1a84e32eabc7f72b844c7211a58c084e
dim.pramatarov@gmail.com <dim.pramatarov@gmail.com>
2021-08-23 05:42:25 +0300

    Intial react app with Apolloclient conected Apolloserver

---
+++ frontend/zeleneya/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div>
+       <h2>My first Apollo app 🚀</h2>
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
